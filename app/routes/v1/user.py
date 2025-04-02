5a1061e36882dc9bcf2a3d3aa28ff4bcc6daf177
NghiemPham <118033992+NghiemPham0404@users.noreply.github.com>
2025-04-02 20:23:17 +0700

    update 2/4/2025
    +thêm chú thích
    +refresh token

--- app/routes/v1/user.py
+++ app/routes/v1/user.py
@@ -8,43 +8,85 @@ from security import bcrypt_context, get_current_user
 
 router = APIRouter(prefix="/users", tags=["users"])
 
+
 @router.post("/", response_model=UserOut)
-async def create_user_endpoint(user_create: UserCreate, db: Session = Depends(get_db)):
-    user = crud.find_user_by_email(db = db, email = user_create.email)
+async def create_user_endpoint(user_create: UserCreate, 
+                               db: Session = Depends(get_db)):
+    # check if user already exists
+    user = crud.find_user_by_email(db = db, 
+                                   email = user_create.email)
     if not user:
+        # create user
         user_in_db = UserInDB(**user_create.model_dump(), hashed_password = bcrypt_context.hash(user_create.password))
         return crud.create(db, user_in_db)
     else:
+        # return error if user already exists
         return HTTPException(status_code=status.HTTP_409_CONFLICT,
                                 detail=f"The user with this {user_create.email} already exists \in the system")
 
+
 @router.get("/", response_model=list[UserOut])
-async def get_users_endpoint(skip: int = 0, limit: int = 20, db: Session = Depends(get_db)):
+async def get_users_endpoint(skip: int = 0, 
+                             limit: int = 20, 
+                             db: Session = Depends(get_db)):
+    """
+    Get users
+    """
     return crud.get_many(db = db, skip=skip, limit=limit)
 
-@router.get("/search", response_model=list[UserOut])
-async def get_users_endpoint(query : str,skip: int = 0, limit: int = 20, db: Session = Depends(get_db)):
+
+@router.get("/search", 
+            response_model=list[UserOut])
+async def get_users_endpoint(query : str,
+                             skip: int = 0, 
+                             limit: int = 20, 
+                             db: Session = Depends(get_db)):
+    """
+    Get users by query
+    """
     return crud.get_many(db, crud._model.name.like(f"%{query}%") , skip=skip, limit=limit)
 
-@router.get("/{user_id}", response_model=UserOut)
-async def get_user_endpoint(user_id: int, db: Session = Depends(get_db)):
+
+@router.get("/{user_id}", 
+            response_model=UserOut)
+async def get_user_endpoint(user_id: int, 
+                            db: Session = Depends(get_db)):
+    """
+    Get user by id
+    """
     user = crud.get_one(db, crud._model.id == user_id)
     if user is None:
         raise HTTPException(status_code=404, detail="User not found")
     return user
     
-@router.put("/{user_id}", response_model=UserOut)
-async def update_user_endpoint(user_id: int, user_update: UserUpdate, db: Session = Depends(get_db)):
+
+@router.put("/{user_id}", 
+            response_model=UserOut)
+async def update_user_endpoint(user_id: int, 
+                               user_update: UserUpdate, 
+                               db: Session = Depends(get_db)):
+    """"
+    Update user by id
+    """
+    # check if user exists in db
     user = crud.get_one(db, crud._model.id == user_id)
     if user is None:
         raise HTTPException(status_code=404, detail="User not found")
+    # update user
     user_in_db = UserInDB(**user_update.model_dump(), hashed_password = bcrypt_context.hash(user_update.password))
     return crud.update(db, user, user_in_db)
 
+
 @router.delete("/{user_id}")
-async def delete_user_endpoint(user_id: int, db: Session = Depends(get_db)):
+async def delete_user_endpoint(user_id: int, 
+                               db: Session = Depends(get_db)):
+    """
+    Delete user by id
+    """
+    # check if user exists in db
     user = crud.get_one(db, crud._model.id == user_id)
     if user is None:
         raise HTTPException(status_code=404, detail="User not found")
+    # delete user
     crud.delete(db, user)
     return {"message": "User deleted"}
--- app/security.py
+++ app/security.py
@@ -12,31 +12,58 @@ from fastapi.security import HTTPAuthorizationCredentials, OAuth2PasswordRequest
 from jose import jwt, JWTError
 from crud.user import crud
 import os
-from dotenv import load_dotenv
 
 http_bearer = HTTPBearer()
 bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated = 'auto')
 
-def create_access_token(email : str, user_id: int, expires_delta: timedelta):
-    encode = {'sub':email, 'id':user_id}
-    expires = datetime.utcnow() + expires_delta
-    encode.update({'exp': expires})
-    return jwt.encode(encode, os.getenv('SECRET_KEY'), algorithm=os.getenv('ALGORITHM'))
+ALGORITHM=os.getenv('ALGORITHM')
+SECRET_KEY=os.getenv('SECRET_KEY')
+ACCESS_TOKEN_EXPIRE_MINUTES=int(os.getenv('ACCESS_TOKEN_EXPIRE_MINUTES'))
+REFRESH_SECRET_KEY=os.getenv('REFRESH_SECRET_KEY')
+REFRESH_TOKEN_EXPIRE_DAYS=int(os.getenv('REFRESH_TOKEN_EXPIRE_DAYS'))
+
+
+
+def create_access_token(email : str, user_id: int):
+    to_encode = {'sub':email, 'id':user_id}
+    expires = datetime.utcnow() +  timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({'exp': expires})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+def create_refresh_token(user_id):
+    to_encode = {'id':user_id}
+    expire = datetime.utcnow() +  timedelta(days=REFRESH_TOKEN_EXPIRE_DAYS)
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, REFRESH_SECRET_KEY, algorithm=ALGORITHM)
 
 async def get_current_user(token_bearer: HTTPAuthorizationCredentials = Depends(http_bearer), db:Session= Depends(get_db)):
     try:
         print(token_bearer.credentials)
         token = token_bearer.credentials 
-        payload = jwt.decode(token, os.getenv('SECRET_KEY'), algorithms=os.getenv('ALGORITHM'))
+        payload = decode_access_token(token)
         email : str = payload.get('sub')
         user_id: int = payload.get('id')
         if email is None or user_id is None:
-            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Could not validate user 1')
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Could not validate user')
         return crud.get_one(db, crud._model.email == email)
         
     except JWTError:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Could not valitdate user 2')
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Could not valitdate user')
     
+def decode_access_token(jwt_bearer_token:str):
+    try:
+        payload = jwt.decode(jwt_bearer_token, SECRET_KEY, algorithms=[ALGORITHM])
+        return payload
+    except JWTError:
+        return None
+    
+def decode_refresh_token(refresh_token:str):
+    try:
+        payload = jwt.decode(refresh_token, REFRESH_SECRET_KEY, algorithms=[ALGORITHM])
+        return payload
+    except JWTError:
+        return None
+
 
 def authenticate_user(email : str, password : str, db):
     user = crud.find_user_by_email(db=db, email=email)
